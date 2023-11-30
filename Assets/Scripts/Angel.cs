5d581b7b683464f1923553137bb8c1f25cb3a97a
Ajamesgames <ajyoung13@hotmail.com>
2023-11-29 21:20:27 -0600

    Added SCP-173/Angel to game for 2nd puzzle

---
+++ Assets/Scripts/Angel.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angel : MonoBehaviour
+{
+    [SerializeField]
+    private float _angelSpeed = 5.0f;
+    [SerializeField]
+    private float _rotateSpeed = 100f;
+    private bool _canMove = true;
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+        if(_player == null)
+        {
+            Debug.LogError("Can't locate Player Tag");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if canmove true, move and rotate towards player
+        if (_canMove == true)
+        {
+            Quaternion _lookRotation = Quaternion.LookRotation(_player.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, _rotateSpeed);
+
+            transform.Translate(Vector3.forward * _angelSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void PlayerLooking()
+    {
+        _canMove = false;
+    }
+
+    public void PlayerNotLooking()
+    {
+        _canMove = true;
+    }
+}
